4979424845d100b8ab04a9c508c3c1c05484c6e6
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2024-12-03 21:24:14 +0900

    2024 1203 homeScreen 수정

--- lib/presentation/view/home_screen.dart
+++ lib/presentation/view/home_screen.dart
@@ -10,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<TaskModel> taskList = [
+    TaskModel(taskTitle: 'Title', date: DateTime.now()),
+  ];
+
   void updateScreen() => setState(() {});
 
   @override
@@ -20,12 +24,37 @@ class _HomeScreenState extends State<HomeScreen> {
         drawer: _drawer(),
         body: Column(
           children: [
-
+            ListView.builder(
+              shrinkWrap: true,
+              physics: AlwaysScrollableScrollPhysics(),
+              itemCount: taskList.length,
+              itemBuilder: (context, index) {
+                return taskWidget(
+                  title: taskList[index].taskTitle,
+                  date: taskList[index].date,
+                );
+              },
+            ),
           ],
         ),
+        floatingActionButton: _floatingActionButton(),
       ),
     );
   }
+
+  FloatingActionButton _floatingActionButton() => FloatingActionButton(onPressed: () {
+
+  },);
+
+  Widget taskWidget({required String title, required DateTime date}) => Container(
+    child: Column(
+      children: [
+        Text(title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+        5.sbH,
+        Text(date.toString(), style: TextStyle(fontSize: 14),),
+      ],
+    ),
+  );
 
   Widget _calendarWidget() => Container();
 
@@ -37,4 +66,15 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: EdgeInsets.only(bottom: 5),
         child: Icon(FeatherIcons.search),
       );
+}
+
+class TaskModel {
+  final String taskTitle;
+  final DateTime date;
+
+  TaskModel({required this.taskTitle, required this.date});
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(taskTitle: json['taskTitle'], date: json['date']);
+
+  Map<String, dynamic> toJson() => {'taskTitle': taskTitle, 'date': date};
 }
