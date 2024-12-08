eb30e469a3a6084a4de1cba47d3e4350de1ffaae
hyejoeng <144520192+hyejoeng@users.noreply.github.com>
2024-12-09 01:29:19 +0900

    2024 1209 로고 수정

--- lib/presentation/view_model/animation_view_model.dart
+++ lib/presentation/view_model/animation_view_model.dart
@@ -6,7 +6,7 @@ class AnimationViewModel extends ChangeNotifier {
 
   Future<void> splashAnimation({required TickerProvider vsync, required void Function() navigatorFunction}) async {
     animationController = AnimationController(vsync: vsync, duration: Duration(seconds: 2))..forward();
-    animation = Tween<double>(begin: 1, end: 0).animate(CurvedAnimation(parent: animationController, curve: Curves.bounceInOut));
+    animation = Tween<double>(begin: 1, end: 0).animate(CurvedAnimation(parent: animationController, curve: Curves.easeInOut));
     animationController.addStatusListener((status) => status == AnimationStatus.completed ? navigatorFunction() : null);
   }
 }
